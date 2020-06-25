7629d9cbcda4562aa902bac189d7bcb405f9007a
Richard Calf <richard.calf@gmail.com>
2020-06-25 21:45:25 +0200

    Introduce IServiceAsync interface

---
+++ password-manager-r2/password.service/IServiceAsync.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password.service
+{
+    public interface IServiceAsync
+    {
+        Task<string> DecryptAsync(string value);
+        Task<string> EncryptAsync(string value);
+    }
+}
