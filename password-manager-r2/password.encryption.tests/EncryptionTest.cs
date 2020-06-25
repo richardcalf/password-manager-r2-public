0af3d2bc5c7d44b4c003568d02e0551a53fdeb41
Richard Calf <richard.calf@gmail.com>
2020-06-25 12:14:52 +0200

    Test project boiler plate

---
+++ password-manager-r2/password.encryption.tests/EncryptionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace password.encryption.tests
+{
+    [TestClass]
+    public class EncryptionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
