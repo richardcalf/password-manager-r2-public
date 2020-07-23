9aa53c98caca1c42b011270aaf9b2e2ef95e82e7
Richard Calf <richard.calf@gmail.com>
2020-07-23 15:33:40 +0200

    Unit tests for db persistence

--- password-manager-r2/password.encryption.tests/ModelTests.cs
+++ password-manager-r2/password.encryption.tests/ModelTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using password.model;
+using password.model.Database;
 
 namespace password.encryption.tests
 {
@@ -57,6 +58,53 @@ namespace password.encryption.tests
                 Assert.IsTrue(!string.IsNullOrEmpty(login.UserName));
                 Assert.IsTrue(login.Password == "a8AzC8EGlsLvyFev5GOI/g==");
             }
+        }
+
+        [TestMethod]
+        public void test_new_database_record()
+        {
+
+            int saves = 0;
+            var login = new LoginModel
+            {
+                UserName = "test.username",
+                Site = "test.com",
+                Password = "Q/NTlrXHkso8AcFGR2AkKw=="
+            };
+            using (var context = new LoginContext())
+            {
+                context.Logins.Add(login);
+                saves = context.SaveChanges();
+            }
+
+            Assert.IsTrue(saves > 0);
+        }
+
+        [TestMethod]
+        public void test_new_database_record_from_plain_jo()
+        {
+            int saves = 0;
+            var loginPojo = new Login
+            {
+                Site = "abc.com",
+                UserName = "richard.calf@gmail.com",
+                Password = "SEjT7fbb6b8kRWPtQ2i/fQ=="
+            };
+
+            var login = new LoginModel
+            {
+                Site = loginPojo.Site,
+                UserName = loginPojo.UserName,
+                Password = loginPojo.Password
+            };
+            using (var context = new LoginContext())
+            {
+                context.Logins.Add(login);
+                saves = context.SaveChanges();
+            }
+
+            Assert.IsTrue(saves > 0);
+
         }
     }
 }
