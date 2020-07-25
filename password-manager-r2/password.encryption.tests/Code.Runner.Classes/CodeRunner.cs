80bff2f7a6c9933a3702b58ad69d782c4a72b644
Richard Calf <richard.calf@gmail.com>
2020-07-25 11:29:11 +0200

    Moving code runner into test classes

---
+++ password-manager-r2/password.encryption.tests/Code.Runner.Classes/CodeRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using password.model;
+using password.service;
+using password.settings;
+
+namespace password.encryption.tests.Code.Runner.Classes
+{
+    public static class CodeRunner
+    {
+        public static IRepository repo = new XmlPersistence();
+        private static Random random = new Random();
+        static string globalSalt = Settings.GetValueFromSettingKey("salt");
+        static IEncryptionService encryptionService = EncryptionServiceFactory.GetEncryptionService(globalSalt);
+
+        public static Login GetRandomLogin()
+        {
+            Login login = new Login();
+            login.Site = RandomString(10);
+            login.UserName = RandomString(10);
+            string plainPassword = RandomString(20);
+            string encryptedPassword = encryptionService.Encrypt(plainPassword);
+            login.Password = encryptedPassword;
+
+            return login;
+        }
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ password-manager-r2/password.encryption.tests/ExecuteCode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using password.encryption.tests.Code.Runner.Classes;
+using password.model;
+
+namespace password.encryption.tests
+{
+    [TestClass]
+    public class ExecuteCode
+    {
+        [TestMethod]
+        public void test_some_code()
+        {
+            List<Login> logins = new List<Login>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var login = CodeRunner.GetRandomLogin();
+                logins.Add(login);
+            }
+            CodeRunner.repo.Save(logins);
+            Assert.IsTrue(true);
+        }
+    }
+}
