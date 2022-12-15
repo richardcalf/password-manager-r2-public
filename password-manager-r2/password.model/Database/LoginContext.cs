d85d75328371038b6df11d1e28043f692523021d
Richard Calf <richard.calf@gmail.com>
2022-12-15 11:58:37 +0200

    filter test complete

--- password-manager-r2/password.model/Database/LoginContext.cs
+++ password-manager-r2/password.model/Database/LoginContext.cs
@@ -7,7 +7,6 @@ namespace password.model.Database
         public LoginContext()
         {
             Database.Connection.ConnectionString = "Server=tcp:password.database.windows.net,1433;Initial Catalog=AligatorFarm;Persist Security Info=False;User ID=richard.calf;Password={DATABASE_PASSWORD};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            //test filter
         }
         public DbSet<LoginModel> Logins { get; set; }
     }
